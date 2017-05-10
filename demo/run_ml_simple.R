97e09dcc1dbd68723cd8475c8d82ae24cfb93266
Luke Winslow <lwinslow@usgs.gov>
2017-05-11 07:33:37 +1200

    First run of variable Kd and TOHA calculation
     1. Added binary of Kd variable glm (just to have around)
     2. Used daily interpolated secchi
     3. Added rough-cut at TOHA calculation

--- demo/run_ml_simple.R
+++ demo/run_ml_simple.R
@@ -1,3 +1,8 @@
+
+lower = initial_params * 0.6
+upper = initial_params * 1.4
+res = optim(fn = tomod, par=initial_params, control=list(parscale=c(initial_params*0.1)))
+
 #first crack at running ML GLM
 library(mda.lakes)
 library(glmtools)
@@ -11,7 +16,7 @@ ll = c(46.244647, -93.652001)
 # write.csv(dvr, 'inst/extdata/ml_nldas_drivers.csv', quote=FALSE, row.names=FALSE)
 
 
-run_ml = function(custom_nml=NULL, fixed_kd, kw_factor=1){
+run_ml = function(custom_nml=NULL, fixed_kd, kw_factor=1, Kw_file=NULL){
   
   run_dir = file.path(tempdir(), 'ml_run')
   dir.create(run_dir)
@@ -32,6 +37,12 @@ run_ml = function(custom_nml=NULL, fixed_kd, kw_factor=1){
                                   area = max(bathy$area))
   
   nml_obj = set_nml(nml_obj, arg_list=c(nml_args, custom_nml))
+  
+  #if we're using variable Kw file, add it to NML
+  if(!is.null(Kw_file)){
+    nml_obj[['glm_setup']]$Kw_file = Kw_file
+  }
+  
   #@jread edits
   # nml_obj = set_nml(nml_obj, 'max_layer_thick', 3)
   # nml_obj = set_nml(nml_obj, 'sw_factor',1.08)
@@ -69,7 +80,12 @@ run_ml = function(custom_nml=NULL, fixed_kd, kw_factor=1){
   
   write_nml(nml_obj, file = file.path(run_dir, 'glm2.nml'))
   
-  run_glm(run_dir)
+  #/run_glm(run_dir)
+  origin = getwd()
+  glmpath = file.path(getwd(), 'inst/extbin/glm')
+  setwd(run_dir)
+  system2(glmpath)
+  setwd(origin)
   
   output = resample_to_field(file.path(run_dir, 'output.nc'), 
                                    field_file = file.path(run_dir, 'obs.tsv'))
@@ -80,7 +96,7 @@ run_ml = function(custom_nml=NULL, fixed_kd, kw_factor=1){
 
 
 
-tomod = function(param){
+tomod = function(param, Kw_file=NULL){
   
   if(any('kw_factor' %in% names(param))){
     kw_factor = param['kw_factor']
@@ -96,7 +112,7 @@ tomod = function(param){
   
   #tryCatch({
     
-    datasp = run_ml(custom_nml=c(custom_nml, fixed_nml), kw_factor=kw_factor)
+    datasp = run_ml(custom_nml=c(custom_nml, fixed_nml), kw_factor=kw_factor, Kw_file=Kw_file)
     
     datasp$error = datasp$Modeled_temp - datasp$Observed_temp
     
@@ -104,6 +120,7 @@ tomod = function(param){
     plot(datasp$Observed_temp, datasp$Modeled_temp, xlab='Observed', ylab='Modeled')
     boxplot(error~floor(Depth), datasp, main=paste(names(param), param, collapse=' ', sep=':'), xlab=paste0('RMSE:', RMSE(datasp[,3:4])))
     abline(h=0)
+  
     return(RMSE(datasp[,3:4]))
   #}, error = function(e){return(NA)})
   
@@ -112,9 +129,34 @@ tomod = function(param){
 
 initial_params = c('cd'=0.0014, 'ce'=0.0014, ch=0.0014, coef_wind_stir=0.376, coef_mix_hyp=0.22, coef_mix_conv=0.2, coef_mix_KH=0.074)
 
-tomod(initial_params)
+tomod(initial_params, Kw_file=file.path(getwd(), 'inst/extdata/ml_model_kd.csv'))
 
-lower = initial_params * 0.6
-upper = initial_params * 1.4
-res = optim(fn = tomod, par=initial_params, control=list(parscale=c(initial_params*0.1)))
 
+
+bathy   = read.table('inst/extdata/ML_hypso.tsv', sep='\t', header=TRUE)
+kd      = read.csv("inst/extdata/ml_model_kd.csv")
+kd$time = as.POSIXct(kd$time, tz='Etc')
+
+names(bathy) = c('depths', 'areas')
+ncfile  = '/tmp/RtmprIheMo/ml_run/output.nc'
+nmlfile  = '/tmp/RtmprIheMo/ml_run/glm2.nml'
+io = get_var(ncfile, 'I_0')
+opt_wtr = get_temp(ncfile, z_out=bathy$depths, reference='surface')
+kd      = subset(kd, time %in% opt_wtr$DateTime)
+
+uyears = unique(lubridate::year(opt_wtr$DateTime))
+out = data.frame()
+for(i in 1:length(uyears)){
+  opti = mda.lakes::opti_thermal_habitat(subset(opt_wtr, lubridate::year(DateTime) == uyears[i]), 
+                                         subset(io, lubridate::year(DateTime) == uyears[i]), 
+                                         subset(kd, lubridate::year(time) == uyears[i])$Kd, ll[1], ll[2], bathy, irr_thresh = c(0.0762, 0.6476), 
+                                  wtr_thresh=c(11,25), interp_hour=TRUE, area_type="benthic")
+  opti$year = uyears[i]
+  out = rbind(opti, out)
+}
+
+write.csv(out, 'out/annual_toha_estimate.csv', row.names=FALSE)
+
+
+mda.lakes::opti_thermal_habitat(opt_wtr, io, kd$Kd, ll[1], ll[2], bathy, irr_thresh = c(0.0762, 0.6476), 
+                                wtr_thresh=c(11,25), interp_hour=FALSE, area_type="benthic")
